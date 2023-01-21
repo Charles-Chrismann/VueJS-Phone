d155a3007e0fb45ef8ed398d2d8527fe8fa8a1b9
unknown <charles.chrismann@free.fr>
2023-01-21 02:25:15 +0100

    :construction: WIP

--- src/views/AddContactView.vue
+++ src/views/AddContactView.vue
@@ -4,6 +4,7 @@ export default {
     data() {
         return {
             formContact: {
+                profilePicture: null,
                 firstName: '',
                 lastName: '',
                 number: ''
@@ -13,35 +14,122 @@ export default {
     methods: {
         addContact() {
             this.$store.state.contacts.push(this.formContact)
+        },
+        showPreview(event){
+          if(event.target.files.length > 0){
+            var src = URL.createObjectURL(event.target.files[0]);
+            // var preview = document.getElementById("file-ip-1-preview");
+            // preview.src = src;
+            // preview.style.display = "block";
+            this.formContact.profilePicture = src
         }
+}
     }
 }
 </script>
 <template lang="">
-    <div>
+    <div class="addContact">
+      <form @submit.prevent="addContact">
         <div class="nav">
             <span class="cancel">
                 <router-link to="/contact">Cancel</router-link>
             </span>
             <h3>New Contact</h3>
-            <span class="done">
-                Done
-            </span>
+            <input class="done" type="submit" value="Done">
         </div>
-        <form @submit.prevent="addContact">
+        
+          
+          <div class="pp">
+            <img v-if="formContact.profilePicture" id="pp-preview" :src="formContact.profilePicture">
+            <img v-else-if="formContact.firstName !== '' || formContact.lastName !== ''" id="pp-preview" :src="`https://ui-avatars.com/api/?background=242424&color=42D392&name=${formContact.firstName !== '' ? formContact.firstName[0] : ''}${formContact.lastName !== '' ? formContact.lastName[0] : ''}&size=256`">
+            <div v-else>
+              <p>add</p>
+              <p>photo</p>
+            </div>
+            <label for="pp"></label>
+            <input type="file" id="pp" accept="image/*" @change="showPreview($event);">
+          </div>
+          <div class="inputs">
+            <input type="text" placeholder="Name..." v-model="formContact.firstName">
+            <input type="text" placeholder="Name..." v-model="formContact.lastName">
+            <input type="tel" placeholder="06" v-model="formContact.number">
+          </div>
 
-        <input type="text" placeholder="Name..." v-model="formContact.firstName">
-        <input type="text" placeholder="Name..." v-model="formContact.lastName">
-        <!-- <span class="error" v-if="formPerso.pseudo.length < 3 && this.error">Pseudo tro cour fdp</span> -->
-        <input type="tel" placeholder="06" v-model="formContact.number">
-        <button type="submit">Ajouter</button>
         </form>
     </div>
 </template>
 <style lang="scss" scoped>
-  .nav {
-    .cancel {
-      color: #0958fc;
+  .addContact {
+    background: #ecebf0;
+    
+    
+    form {
+      background: #fff;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      .nav {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        .cancel {
+          color: #0958fc;
+        }
+  
+        .done {
+          background: none;
+          border: none;
+          outline: none;
+  
+        }
+      }
+      .pp {
+        position: relative;
+        width: 50%;
+        aspect-ratio: 1/1;
+        border-radius: 50%;
+        overflow: hidden;
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          object-position: center;
+        }
+
+        & > div {
+          width: 100%;
+          height: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-direction: column;
+        }
+
+        label {
+          position: absolute;
+          inset: 0;
+        }
+
+        input {
+          display: none;
+        }
+      }
+
+      .inputs {
+        border-top: solid  1px #d7d7d7;
+        border-bottom: solid  1px #d7d7d7;
+        input {
+          border: none;
+          outline: none;
+          padding-left: 5%;
+          width: 95%;
+          border-bottom: solid  1px #d7d7d7;
+
+          &:last-child {
+            border-bottom: none;
+          }
+        }
+      }
     }
   }
 </style>
